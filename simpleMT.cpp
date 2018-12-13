f2edd3a06231a0e671a66a935d3698834b67e166
aleronego <aleronego@gmail.com>
2018-12-13 18:02:17 +0200

    Update simple multithread example

--- simpleMT.cpp
+++ simpleMT.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 using namespace std;
+
+void Work()
+{
+	for(int i = 0; i < 10; ++i)
+	{
+		cout << "T id " << this_thread::get_id() << ", i = " << i << endl;
+		this_thread::sleep_for(chrono::milliseconds(1000));
+	}
+}
+
 
 int main()
 {
-   cout << "Hello, multithread world!\n";
-   return 0;
+	thread th(Work);
+
+	for(int i = 0; i < 10; ++i)
+	{
+		cout << "M id " << this_thread::get_id() << ", i = " << i << endl;
+		this_thread::sleep_for(chrono::milliseconds(500));
+	}
+
+	th.join();
+	cout << "Thread joined!" << endl;
+	return 0;
 }
