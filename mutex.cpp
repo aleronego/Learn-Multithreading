2a12d328900738f0829318d2360bf59fafc1569d
aleronego <aleronego@gmail.com>
2018-12-17 16:19:23 +0200

    Change codestyle

--- mutex.cpp
+++ mutex.cpp
@@ -1,33 +1,31 @@
-#include <iostream>
-#include <thread>
-#include <mutex>
 #include <chrono>
+#include <iostream>
+#include <mutex>
+#include <thread>
 
 using namespace std;
 
 mutex mtx;
 void PrintBox(char ch)
 {
-	mtx.lock();
-	for(int i = 0; i < 10; ++i)
-	{
-		for(int j = 0; j < 10; ++j)
-		{
-			cout << ch;
-			this_thread:: sleep_for(chrono::milliseconds(20));
-		}
-		cout << endl;
-	}
-	cout << endl << endl;
-	mtx.unlock();
+    mtx.lock();
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            cout << ch;
+            this_thread::sleep_for(chrono::milliseconds(20));
+        }
+        cout << endl;
+    }
+    cout << endl
+         << endl;
+    mtx.unlock();
 }
 
 int main()
 {
-	thread th1(PrintBox, '*');
-	thread th2(PrintBox, '#');
+    thread th1(PrintBox, '*');
+    thread th2(PrintBox, '#');
 
-	th1.join();
-	th2.join();
+    th1.join();
+    th2.join();
 }
-
